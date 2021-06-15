1396fa82efd616c86fd8b9115a15091dc25afb78
Tony <arinaldi@vt.edu>
2021-06-15 08:54:18 -0500

    Update Admin TableSkeleton

--- components/TableSkeleton.tsx
+++ components/TableSkeleton.tsx
@@ -4,18 +4,18 @@ const threeRows = Array.from({ length: 3 }, (_, i) => i);
 const sevenCols = Array.from({ length: 7 }, (_, i) => i);
 
 const TableSkeleton: FC = () => (
-  <tbody className="bg-white divide-y divide-gray-200">
+  <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-500 dark:divide-black">
     {threeRows.map(row => (
       <tr
-        className="odd:bg-gray-50"
+        className="odd:bg-gray-50 dark:odd:bg-gray-400"
         key={row}
       >
         {sevenCols.map(cell => (
           <td
-            className="px-3 py-2 whitespace-nowrap text-sm text-gray-900"
+            className="animate-pulse h-12 px-3 py-2"
             key={cell}
           >
-            —
+            <div className="h-3 bg-gray-300 rounded w-2/3" />
           </td>
         ))}
       </tr>
