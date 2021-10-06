4ee822072258cb9f428b4c7fc05a0d1a96137f2f
Tony Rinaldi <tony.rinaldi13@gmail.com>
2021-10-06 18:02:05 -0500

    react-hook-form already adds name to input

--- components/Input.tsx
+++ components/Input.tsx
@@ -22,7 +22,6 @@ const Input: FC<Props> = ({ id, inputRef, required, type, ...rest }) => {
         autoComplete={id}
         className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md dark:border-black dark:bg-gray-700 dark:text-white"
         id={id}
-        name={id}
         ref={(e) => inputRef(e)}
         required={required}
         type={type}
