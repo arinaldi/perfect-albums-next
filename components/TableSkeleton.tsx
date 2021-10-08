8cb26d253e1d81d3e301593127aef930b3b0614b
Tony Rinaldi <tony.rinaldi13@gmail.com>
2021-10-08 13:55:51 -0500

    Update styles for TableSkeleton

--- components/TableSkeleton.tsx
+++ components/TableSkeleton.tsx
@@ -5,9 +5,12 @@ const sevenCols = Array.from({ length: 7 }, (_, i) => i);
 
 const TableSkeleton: FC = () => (
   <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-500 dark:divide-black">
-    {threeRows.map(row => (
-      <tr className="odd:bg-gray-50 dark:odd:bg-gray-400" key={row}>
-        {sevenCols.map(cell => (
+    {threeRows.map((row) => (
+      <tr
+        className="odd:bg-gray-100 even:bg-gray-0 dark:odd:bg-gray-700 dark:even:bg-gray-800"
+        key={row}
+      >
+        {sevenCols.map((cell) => (
           <td className="animate-pulse h-12 px-3 py-2" key={cell}>
             <div className="h-3 bg-gray-300 rounded w-2/3" />
           </td>
