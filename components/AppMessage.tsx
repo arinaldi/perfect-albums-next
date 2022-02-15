f5a21d114db2f6f1f0858cec59d8af3237d66b9f
Tony Rinaldi <tony.rinaldi13@gmail.com>
2022-02-15 14:39:58 -0600

    Update styles for AppMessage

--- components/AppMessage.tsx
+++ components/AppMessage.tsx
@@ -1,27 +1,40 @@
+import {
+  ExclamationCircleIcon,
+  InformationCircleIcon,
+} from '@heroicons/react/solid';
+
 import { APP_MESSAGE_TYPES } from 'constants/index';
 
-const { ERROR } = APP_MESSAGE_TYPES;
+const { ERROR, INFO } = APP_MESSAGE_TYPES;
 
 interface Props {
   message?: string;
   type?: APP_MESSAGE_TYPES;
 }
 
+const styles = {
+  [ERROR]: 'bg-red-100 border-red-700 text-red-700',
+  [INFO]: 'bg-blue-100 border-blue-700 text-blue-700',
+};
+
+const className = 'w-6 h-6';
+const icons = {
+  [ERROR]: <ExclamationCircleIcon className={className} />,
+  [INFO]: <InformationCircleIcon className={className} />,
+};
+
 export default function AppMessage({
   message = 'Something went wrong',
   type = ERROR,
 }: Props) {
   return (
-    <div className="mt-8 flex justify-center">
+    <div className="mt-8 text-center">
       <div
-        className={`rounded-sm border-l-4 px-6 py-4 ${
-          type === ERROR
-            ? 'border-red-700 bg-red-200'
-            : 'border-blue-700 bg-blue-200'
-        }`}
-        style={{ height: 'fit-content' }}
+        className={`inline-flex items-center rounded border-l-4 py-4 pl-5 pr-6 ${styles[type]}`}
+        role="alert"
       >
-        <p className="text-center text-lg">{message}</p>
+        {icons[type]}
+        <p className="ml-3 text-center">{message}</p>
       </div>
     </div>
   );
