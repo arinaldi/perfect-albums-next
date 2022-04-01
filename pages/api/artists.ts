2919ae67478dbf7dcaa71ca23962a1489436ad69
Tony Rinaldi <tony.rinaldi13@gmail.com>
2022-04-01 13:42:50 -0500

    PR comments

--- pages/api/artists.ts
+++ pages/api/artists.ts
@@ -1,14 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { supabaseServerClient } from '@supabase/supabase-auth-helpers/nextjs';
+import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs';
 
-async function getArtists(
-  req: NextApiRequest,
-  res: NextApiResponse,
-): Promise<string[]> {
+async function getArtists(): Promise<string[]> {
   // https://www.jeffreyknox.dev/blog/postgresql-functions-in-supabase/
-  const { data: artists, error } = await supabaseServerClient({ req, res }).rpc(
-    'get_artists',
-  );
+  const { data: artists, error } = await supabaseClient.rpc('get_artists');
 
   if (error) throw error;
   if (artists) return artists.map((a) => a.artist);
@@ -20,7 +15,7 @@ export default async function artists(
   res: NextApiResponse,
 ): Promise<void> {
   try {
-    const artists = await getArtists(req, res);
+    const artists = await getArtists();
     res.status(200).json({ success: true, artists });
   } catch (error) {
     res.status(400).json({ success: false });
