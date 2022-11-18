10d6aee53a28032d90c51eb4248d10e8080e0220
Tony Rinaldi <tony.rinaldi13@gmail.com>
2022-11-18 10:04:53 -0600

    Redirect from signin to admin if session exists

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { NextRequest, NextResponse } from 'next/server';
-import { ROUTE_HREF } from 'utils/constants';
+import { ROUTE_HREF, ROUTES_ADMIN } from 'utils/constants';
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
@@ -15,7 +15,13 @@ export async function middleware(req: NextRequest) {
     return NextResponse.redirect(url);
   }
 
-  if (!session && req.nextUrl.pathname.startsWith('/admin')) {
+  if (session && req.nextUrl.pathname === ROUTE_HREF.SIGNIN) {
+    const url = req.nextUrl.clone();
+    url.pathname = ROUTES_ADMIN.base.href;
+    return NextResponse.redirect(url);
+  }
+
+  if (!session && req.nextUrl.pathname.startsWith(ROUTES_ADMIN.base.href)) {
     const url = req.nextUrl.clone();
     url.pathname = ROUTE_HREF.TOP_ALBUMS;
     return NextResponse.redirect(url);
