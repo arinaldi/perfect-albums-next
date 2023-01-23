20d6ccf3e0d1090fb5d9f7349b434e24198716bb
Tony Rinaldi <tony.rinaldi13@gmail.com>
2023-01-23 11:02:35 -0600

    Update button styles

--- components/OutlineButton.tsx
+++ components/OutlineButton.tsx
@@ -5,11 +5,11 @@ interface Props {
   onClick: () => void;
 }
 
-const CancelButton = forwardRef<HTMLButtonElement, Props>(
+const OutlineButton = forwardRef<HTMLButtonElement, Props>(
   ({ children, onClick, ...rest }, ref) => {
     return (
       <button
-        className="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none dark:border-gray-600 dark:bg-transparent dark:text-white dark:hover:bg-gray-900"
+        className="inline-flex min-w-[96px] items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-semibold text-gray-500 shadow-sm hover:bg-gray-50 hover:shadow-md focus:border-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:border-gray-600 dark:bg-gray-600 dark:text-white dark:hover:bg-gray-700"
         onClick={onClick}
         ref={ref}
         type="button"
@@ -21,6 +21,6 @@ const CancelButton = forwardRef<HTMLButtonElement, Props>(
   },
 );
 
-CancelButton.displayName = 'CancelButton';
+OutlineButton.displayName = 'OutlineButton';
 
-export default CancelButton;
+export default OutlineButton;
--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -7,7 +7,7 @@ interface Props {
 export default function SubmitButton({ isSubmitting }: Props) {
   return (
     <button
-      className="inline-flex min-w-[96px] items-center justify-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none active:bg-gray-700 disabled:cursor-not-allowed disabled:opacity-50"
+      className="inline-flex min-w-[96px] items-center justify-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 hover:shadow-md focus:border-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-700 disabled:cursor-not-allowed disabled:opacity-50"
       disabled={isSubmitting}
       type="submit"
     >
