abd994738adcf3be90ddac4786109670da1668ea
Tony Rinaldi <tony.rinaldi13@gmail.com>
2023-08-31 15:01:08 -0500

    Debounce admin search fields

---
+++ app/admin/InputSpinner.tsx
@@ -0,0 +1,9 @@
+import Spinner from 'components/Spinner';
+
+export default function InputSpinner() {
+  return (
+    <div className="absolute bottom-0 right-0 top-0 flex items-center justify-center">
+      <Spinner className="mr-1.5 h-6 w-6 cursor-none p-1" />
+    </div>
+  );
+}
