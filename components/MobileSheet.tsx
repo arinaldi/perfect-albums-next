a1b94f127cdc7c0cb7f59aa3927e3a1392bbeae2
Tony Rinaldi <tony.rinaldi13@gmail.com>
2024-03-07 09:22:16 -0600

    Add avatar to mobile sheet

--- components/MobileSheet.tsx
+++ components/MobileSheet.tsx
@@ -4,6 +4,7 @@ import { usePathname } from 'next/navigation';
 import { HamburgerMenuIcon } from '@radix-ui/react-icons';
 import { type User } from '@supabase/supabase-js';
 
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import {
   Sheet,
@@ -57,53 +58,71 @@ export function MobileSheet({ signOut, user }: Props) {
             The best music on the net
           </SheetDescription>
         </SheetHeader>
-        <nav className="mt-8 flex flex-col items-start gap-6">
-          {ROUTES.map((r) => (
-            <Link
-              className={cn(
-                'text-lg font-medium transition-colors hover:text-primary',
-                !pathname?.startsWith(r.href) ? 'text-muted-foreground' : '',
-              )}
-              key={r.href}
-              href={r.href}
-              onClick={onClose}
-            >
-              {r.label}
-            </Link>
-          ))}
+        <nav className="flex h-full flex-col items-start justify-between pb-16 pt-8">
+          <div className="flex flex-col items-start gap-6">
+            {ROUTES.map((r) => (
+              <Link
+                className={cn(
+                  'text-lg font-medium transition-colors hover:text-primary',
+                  !pathname?.startsWith(r.href) ? 'text-muted-foreground' : '',
+                )}
+                key={r.href}
+                href={r.href}
+                onClick={onClose}
+              >
+                {r.label}
+              </Link>
+            ))}
+            {user ? (
+              <>
+                <Link
+                  className={cn(
+                    'text-lg font-medium transition-colors hover:text-primary',
+                    !pathname?.startsWith(ROUTES_ADMIN.base.href)
+                      ? 'text-muted-foreground'
+                      : '',
+                  )}
+                  href={ROUTES_ADMIN.base.href}
+                  onClick={onClose}
+                >
+                  {ROUTES_ADMIN.base.label}
+                </Link>
+                <Button
+                  className="flex h-auto items-center gap-3 p-0 text-lg text-muted-foreground hover:bg-transparent"
+                  onClick={() => {
+                    onClose();
+                    signOut();
+                  }}
+                  variant="ghost"
+                >
+                  Sign out
+                </Button>
+              </>
+            ) : (
+              <Link
+                className="text-lg font-medium text-muted-foreground transition-colors hover:text-primary"
+                href={ROUTE_HREF.SIGNIN}
+                onClick={onClose}
+              >
+                Sign in
+              </Link>
+            )}
+          </div>
           {user && (
-            <Link
-              className={cn(
-                'text-lg font-medium transition-colors hover:text-primary',
-                !pathname?.startsWith(ROUTES_ADMIN.base.href)
-                  ? 'text-muted-foreground'
-                  : '',
-              )}
-              href={ROUTES_ADMIN.base.href}
-              onClick={onClose}
-            >
-              {ROUTES_ADMIN.base.label}
-            </Link>
-          )}
-          {user ? (
-            <Button
-              className="h-auto p-0 text-lg text-muted-foreground hover:bg-transparent"
-              onClick={() => {
-                onClose();
-                signOut();
-              }}
-              variant="ghost"
-            >
-              Sign out
-            </Button>
-          ) : (
-            <Link
-              className="text-lg font-medium text-muted-foreground transition-colors hover:text-primary"
-              href={ROUTE_HREF.SIGNIN}
-              onClick={onClose}
-            >
-              Sign in
-            </Link>
+            <div className="flex items-center gap-3">
+              <Avatar>
+                <AvatarImage src="/avatars/02.png" />
+                <AvatarFallback>
+                  {`${user.user_metadata.firstName[0]}${user.user_metadata.lastName[0]}`}
+                </AvatarFallback>
+              </Avatar>
+              <div>
+                <p className="text-sm font-medium">
+                  {`${user.user_metadata.firstName} ${user.user_metadata.lastName}`}
+                </p>
+                <p className="text-xs text-muted-foreground">{user.email}</p>
+              </div>
+            </div>
           )}
         </nav>
       </SheetContent>
