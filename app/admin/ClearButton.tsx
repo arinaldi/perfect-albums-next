7722cc50770229a657fd85591c16ad0ad559fd92
Tony <arinaldi@vt.edu>
2024-04-27 08:23:50 -0500

    restyle input clear button

--- app/admin/ClearButton.tsx
+++ app/admin/ClearButton.tsx
@@ -1,15 +1,18 @@
 import { Cross2Icon } from '@radix-ui/react-icons';
 
+import { Button } from '@/components/ui/button';
+
 interface ButtonProps {
   onClick: () => void;
 }
 
 export default function ClearButton({ onClick }: ButtonProps) {
   return (
-    <div className="absolute bottom-0 right-0 top-0 flex items-center justify-center">
-      <span className="mr-1.5 size-6 cursor-pointer rounded-full p-1 transition hover:bg-gray-100">
-        <Cross2Icon onClick={onClick} />
-      </span>
-    </div>
+    <Button
+      className="absolute bottom-0 right-0 top-0 flex items-center justify-center p-2.5"
+      variant="ghost"
+    >
+      <Cross2Icon onClick={onClick} />
+    </Button>
   );
 }
