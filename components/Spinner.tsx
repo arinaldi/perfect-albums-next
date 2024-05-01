8ff7fc96780b9d32b945826a333ec2fccbc4d737
Tony <arinaldi@vt.edu>
2024-04-30 19:32:09 -0500

    update icon for Spinner component

--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,4 +1,6 @@
-import { cn } from 'lib/utils';
+import { ReloadIcon } from '@radix-ui/react-icons';
+
+import { cn } from '@/lib/utils';
 
 interface Props {
   className?: string;
@@ -6,27 +8,8 @@ interface Props {
 
 export default function Spinner({ className = '' }: Props) {
   return (
-    <div className={cn('size-6', className)}>
-      <svg
-        className="animate-spin duration-500"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          className="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          strokeWidth="4"
-        ></circle>
-        <path
-          className="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
-    </div>
+    <ReloadIcon
+      className={cn('inline-block size-6 animate-spin duration-500', className)}
+    />
   );
 }
