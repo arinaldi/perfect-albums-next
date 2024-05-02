f4e2a390bd0c8e6dcdb9a37c3e672c31fbf03874
Tony <arinaldi@vt.edu>
2024-05-02 18:50:13 -0500

    modal tinkering 2

--- app/releases/AddReleaseModal.tsx
+++ app/releases/AddReleaseModal.tsx
@@ -14,17 +14,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from '@/components/ui/drawer';
-import { useMediaQuery } from '@/hooks/media-query';
 import { releaseSchema, type ReleaseInput } from './schema';
 import { addRelease } from './actions';
 import ReleaseForm from './ReleaseForm';
@@ -37,7 +26,6 @@ const defaultValues = {
 
 export default function AddReleaseModal() {
   const [open, setOpen] = useState(false);
-  const isDesktop = useMediaQuery();
   const form = useForm<ReleaseInput>({
     defaultValues,
     resolver: zodResolver(releaseSchema),
@@ -61,53 +49,22 @@ export default function AddReleaseModal() {
     successMessage: `${MESSAGES.RELEASE_PREFIX} added`,
   });
 
-  if (isDesktop) {
-    return (
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger asChild>
-          <Button>Add release</Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader className="text-left">
-            <DialogTitle>Add release</DialogTitle>
-            <DialogDescription>
-              What&apos;s the newest release?
-            </DialogDescription>
-          </DialogHeader>
-          <ReleaseForm
-            form={form}
-            isSubmitting={isSubmitting}
-            onSubmit={onSubmit}
-          />
-        </DialogContent>
-      </Dialog>
-    );
-  }
-
   return (
-    <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerTrigger asChild>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
         <Button>Add release</Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader className="text-left">
-          <DrawerTitle>Add release</DrawerTitle>
-          <DrawerDescription>What&apos;s the newest release?</DrawerDescription>
-        </DrawerHeader>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader className="text-left">
+          <DialogTitle>Add release</DialogTitle>
+          <DialogDescription>What&apos;s the newest release?</DialogDescription>
+        </DialogHeader>
         <ReleaseForm
-          className="px-4"
           form={form}
           isSubmitting={isSubmitting}
           onSubmit={onSubmit}
         />
-        <DrawerFooter className="pt-2">
-          <DrawerClose asChild>
-            <Button size="lg" variant="outline">
-              Cancel
-            </Button>
-          </DrawerClose>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
+      </DialogContent>
+    </Dialog>
   );
 }
--- app/songs/AddSongModal.tsx
+++ app/songs/AddSongModal.tsx
@@ -14,17 +14,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from '@/components/ui/drawer';
-import { useMediaQuery } from '@/hooks/media-query';
 import { songSchema, type SongInput } from './schema';
 import { addSong } from './actions';
 import SongForm from './SongForm';
@@ -37,7 +26,6 @@ const defaultValues = {
 
 export default function AddSongModal() {
   const [open, setOpen] = useState(false);
-  const isDesktop = useMediaQuery();
   const form = useForm<SongInput>({
     defaultValues,
     resolver: zodResolver(songSchema),
@@ -61,55 +49,20 @@ export default function AddSongModal() {
     successMessage: `${MESSAGES.SONG_PREFIX} added`,
   });
 
-  if (isDesktop) {
-    return (
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger asChild>
-          <Button>Add song</Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader className="text-left">
-            <DialogTitle>Add song</DialogTitle>
-            <DialogDescription>
-              What&apos;s the next featured song?
-            </DialogDescription>
-          </DialogHeader>
-          <SongForm
-            form={form}
-            isSubmitting={isSubmitting}
-            onSubmit={onSubmit}
-          />
-        </DialogContent>
-      </Dialog>
-    );
-  }
-
   return (
-    <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerTrigger asChild>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
         <Button>Add song</Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader className="text-left">
-          <DrawerTitle>Add song</DrawerTitle>
-          <DrawerDescription>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader className="text-left">
+          <DialogTitle>Add song</DialogTitle>
+          <DialogDescription>
             What&apos;s the next featured song?
-          </DrawerDescription>
-        </DrawerHeader>
-        <SongForm
-          className="px-4"
-          form={form}
-          isSubmitting={isSubmitting}
-          onSubmit={onSubmit}
-        />
-        <DrawerFooter className="pt-2">
-          <DrawerClose asChild>
-            <Button size="lg" variant="outline">
-              Cancel
-            </Button>
-          </DrawerClose>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
+          </DialogDescription>
+        </DialogHeader>
+        <SongForm form={form} isSubmitting={isSubmitting} onSubmit={onSubmit} />
+      </DialogContent>
+    </Dialog>
   );
 }
