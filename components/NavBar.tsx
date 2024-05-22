4bae62106946cb3669c76f99502a77213fdb42e5
Tony Rinaldi <tony.rinaldi13@gmail.com>
2024-05-22 14:23:24 -0500

    sticky nav bar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -40,7 +40,7 @@ export default function NavBar({ user }: Props) {
   }
 
   return (
-    <div className="border-b">
+    <div className="sticky top-0 z-10 border-b bg-background">
       {/* Desktop */}
       <div className="hidden h-14 items-center justify-between px-5 sm:flex xl:px-8">
         <nav className="flex items-center gap-6">
